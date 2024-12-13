7cad926ae477b7d6a2c1e93363f7567e7359684c
Jakub Pesser <jakub.pesser.022@pslib.cz>
2024-12-13 09:30:13 +0100

    Models

---
+++ Edwinschoice.Server/Models/Items.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Edwinschoice.Server.Models
+{
+    public class Items
+    {
+        public int ItemId { get; set; }
+        public required string ItemName { get; set; }
+        public int? Health { get; set; }
+        public int? Attack {  get; set; }
+        public int? Defense { get; set; }
+        public bool isConsumable { get; set; }
+        public bool forStory { get; set; }
+        public required byte[] Image { get; set; }
+    }
+}
---
+++ Edwinschoice.Server/Models/Locations.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Edwinschoice.Server.Models
+{
+    public class Locations : Items
+    {
+        public int LocationId { get; set; }
+        public required string LocationName { get; set; }
+        public required string Description { get; set; }
+        public required byte[] BackgroundImage { get; set; }
+        public bool Reobtainable { get; set; }
+        public Items? ItemId { get; set; }
+
+    }
+}
